fd0fe074311ae284cfc33bbb19cd9c20752057e3
Alex Poputa <poputa.alex@gmail.com>
2024-08-15 16:05:46 +0300

    started Task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,8 @@
 import flatpickr from "flatpickr";
 
-flatpickr('input#datetime-picker', {
-    enableTime:true,
-    time_24hr:true,
-    allowInput:true,
-    altFormat:"d M Y H:i",
-    dateFormat: 'Y-m-d H:i'
-});
+flatpickr('#datetime-picker',{
+    altInput: true,
+    enableTime: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+})
