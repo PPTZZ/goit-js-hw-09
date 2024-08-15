666d8d7e024c2efcf828e21b93950754b75c8b56
Alex Poputa <poputa.alex@gmail.com>
2024-08-15 14:21:28 +0300

    finished Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.querySelector('body');
+const strtBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const handleStart = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  strtBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+const handleStop = () => {
+  clearInterval(interval);
+  strtBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+strtBtn.addEventListener('click', handleStart);
+stopBtn.addEventListener('click', handleStop);
