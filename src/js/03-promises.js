52977b5499b30e41463db2be30f6689491ab81cb
Alex Poputa <poputa.alex@gmail.com>
2024-08-17 14:09:50 +0300

    Finished Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
+  return new Promise((res, rej) => {
+    setTimeout(()=>{
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      res({ position, delay });
+    } else {
+      rej({ position, delay });
+    }
+  },delay)
+  });
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = parseInt(e.target.delay.value);
+  const step = parseInt(e.target.step.value);
+  const amount = parseInt(e.target.amount.value);
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = delay + i * step;
+    createPromise(i + 1, currentDelay)
+      .then(value => {
+        Notify.success(
+          `✅ Fulfilled promise ${value.position} in ${value.delay}ms`
+        );
+      })
+      .catch(err => {
+        Notify.failure(`❌ Rejected promise ${err.position} in ${err.delay}ms`);
+      });
   }
-}
+});
