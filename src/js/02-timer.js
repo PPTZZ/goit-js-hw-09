979eb4c9c80637918eb0d5ba3f3101deabcdac98
Alex Poputa <poputa.alex@gmail.com>
2024-08-17 12:42:14 +0300

    Finished Task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,30 @@
+import { Report } from 'notiflix';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+// Selectors
 const startBtn = document.querySelector('button[data-start]');
 const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const mins = document.querySelector('span[data-minutes]');
 const secs = document.querySelector('span[data-seconds]');
 
+// CSS Selector & options
+const addCSS = css =>
+  (document.head.appendChild(document.createElement('style')).innerHTML = css);
+addCSS(
+  '.timer{height:80vh;display:flex;justify-content:center;align-items:center;gap:30px;font-size:50px;}'
+);
+
+// Variables
 let interval;
 let today;
 let timer;
+
+// Start with disabled button
 startBtn.disabled = true;
 
+// flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -21,15 +34,11 @@ const options = {
     today = new Date();
     if (selectedDates[0].getTime() - today.getTime() <= 0) {
       selectedDates[0] = today;
-      alert('Please choose a date in the future');
+      Report.failure('Invalid Date','Please pick a date from the future','OK',()=>window.location.reload())
     } else {
       timer = selectedDates[0].getTime() - today.getTime();
       startBtn.disabled = false;
       clearInterval(interval);
-      days.innerText = '00';
-      hours.innerText = '00';
-      mins.innerText = '00';
-      secs.innerText = '00';
     }
   },
 };
@@ -51,25 +60,31 @@ const remainingTime = timer => {
   return { reDays, reHours, reMinutes, reSeconds };
 };
 
-const addLeadingZero = (value)=>{
-    const keys = Object.keys(remainingTime(value));
-    for (const key of keys) {
-        let digits = remainingTime(value)[key].toString()
-        if (digits.length === 1){
-            
-        }
-      }
-}
+// Adding 0
+const addLeadingZero = value => {
+  if (value < 10) {
+    return value.toString().padStart(2, '0');
+  } else {
+    return value;
+  }
+};
 
-
+// Timer
 startBtn.addEventListener('click', () => {
-    addLeadingZero(timer);
   interval = setInterval(() => {
     const { reDays, reHours, reMinutes, reSeconds } = remainingTime(timer);
-    days.innerText = reDays;
-    hours.innerText = reHours;
-    mins.innerText = reMinutes;
-    secs.innerText = reSeconds;
+    days.innerText = addLeadingZero(reDays);
+    hours.innerText = addLeadingZero(reHours);
+    mins.innerText = addLeadingZero(reMinutes);
+    secs.innerText = addLeadingZero(reSeconds);
+    if (
+      reDays == 0 &&
+      reHours == 0 &&
+      reMinutes == 0 &&
+      Number(reSeconds) < 10
+    ) {
+      addCSS('.timer{color:red;}');
+    }
     timer -= 1000;
     if (timer <= 0) {
       clearInterval(interval);
